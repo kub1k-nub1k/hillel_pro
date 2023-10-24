0faa533ae3f6e707aa77fbdc3187663882c2d00a
artempindich <artempindich@gmail.com>
2023-10-24 21:04:26 +0300

    add home_work1 and main.py

--- home_work1/main.py
+++ home_work1/main.py
@@ -1,20 +1,31 @@
 def parse(query: str) -> dict:
-    return {}
+    params = query.split('?')[-1].split('&')
+    result = {}
+    for param in params:
+        if '=' in param:
+            key, value = param.split('=')
+            result[key] = value
+    return result
 
 
-if __name__ == '__main__':
+def parse_cookie(query: str) -> dict:
+    result = {}
+    cookies = query.split(';')
+    for cookie in cookies:
+        key_value = cookie.strip().split('=', 1)
+        if len(key_value) == 2:
+            key, value = key_value
+            result[key.strip()] = value
+    return result
+
+
+if __name__ == '__main':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=John') == {'name': 'John'}
 
-
-def parse_cookie(query: str) -> dict:
-    return {}
-
-
-if __name__ == '__main__':
     assert parse_cookie('name=John;') == {'name': 'John'}
     assert parse_cookie('') == {}
     assert parse_cookie('name=John;age=28;') == {'name': 'John', 'age': '28'}
